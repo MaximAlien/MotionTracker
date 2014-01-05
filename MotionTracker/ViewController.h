b95a2c2ce70286b4b03e06bd6b4a264eefcb11d0
MaximAlien <maxim.makhun@gmail.com>
2014-01-05 15:37:34 +0200

    Intermediate commit. Fixing issues with accuracy and distance

--- MotionTracker/ViewController.h
+++ MotionTracker/ViewController.h
@@ -13,7 +13,10 @@
 @interface ViewController : UIViewController <CLLocationManagerDelegate, MKMapViewDelegate>
 {
     NSInteger todayStepsCount;
-    NSInteger totalPoints;
+    CLLocation *startingLocation;
+    CLLocation *lastRecordedLocation;
+    CLLocationDistance totalDistance;
+    NSTimeInterval lastDistanceCalculation;
 }
 
 @property (weak, nonatomic) IBOutlet UILabel *totalSteps;
@@ -23,9 +26,11 @@
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UILabel *totalStepsYesterday;
 - (IBAction)zoomBackToUserLocation:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *distanceTravelled;
 
 @property (strong, nonatomic) MKPolylineView* routeLineView;
 @property (strong, nonatomic) NSMutableArray *locations;
+@property (strong, nonatomic) NSMutableArray *locationHistory;
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (nonatomic, readwrite) MKMapRect routeRect;
 @property (strong, nonatomic) MKPolyline* routeLine;
--- MotionTracker/ViewController.m
+++ MotionTracker/ViewController.m
@@ -14,13 +14,20 @@
 
 @end
 
+#define kDistanceCalculationInterval 10 // the interval (seconds) at which we calculate the user's distance
+#define kNumLocationHistoriesToKeep 5 // the number of locations to store in history so that we can look back at them and determine which is most accurate
+#define kValidLocationHistoryDeltaInterval 3 // the maximum valid age in seconds of a location stored in the location history
+#define kMinLocationsNeededToUpdateDistance 3 // the number of locations needed in history before we will even update the current distance
+#define kRequiredHorizontalAccuracy 40.0f // the required accuracy in meters for a location.  anything above this number will be discarded
+
+
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    totalPoints = 0;
+    self.firstTime = YES;
     
     if ([CMStepCounter isStepCountingAvailable])
     {
@@ -56,7 +63,7 @@
         
         NSOperationQueue *queue = [NSOperationQueue new];
         queue.name = @"Step Counter Queue";
-
+        
         [self.stepCounter startStepCountingUpdatesToQueue:queue updateOn:1 withHandler:^(NSInteger numberOfSteps, NSDate *timestamp, NSError *error) {
             dispatch_async(dispatch_get_main_queue(),^{
                 NSInteger stepsCount = todayStepsCount + numberOfSteps;
@@ -81,13 +88,55 @@
     
     self.locations = [NSMutableArray array];
     
-    if ([CLLocationManager locationServicesEnabled]) {
-        _locationManager = [[CLLocationManager alloc] init];
-        
-        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-        _locationManager.delegate = self;
-        
-        [_locationManager startUpdatingLocation];
+    if (![CLLocationManager locationServicesEnabled])
+    {
+        NSLog(@"location services are disabled");
+        return;
+    }
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied)
+    {
+        NSLog(@"location services are blocked by the user");
+        return;
+    }
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorized)
+    {
+        NSLog(@"location services are enabled");
+    }
+    
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined)
+    {
+        NSLog(@"about to show a dialog requesting permission");
+    }
+    
+    self.locationManager = [CLLocationManager new];
+    self.locationManager.delegate = self;
+    
+    /* Pinpoint our location with the following accuracy:
+     *
+     *     kCLLocationAccuracyBestForNavigation  highest + sensor data
+     *     kCLLocationAccuracyBest               highest
+     *     kCLLocationAccuracyNearestTenMeters   10 meters
+     *     kCLLocationAccuracyHundredMeters      100 meters
+     *     kCLLocationAccuracyKilometer          1000 meters
+     *     kCLLocationAccuracyThreeKilometers    3000 meters
+     */
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+    
+    /* Notify changes when device has moved x meters.
+     * Default value is kCLDistanceFilterNone: all movements are reported.
+     */
+    self.locationManager.distanceFilter = 5.0f;
+    
+    /* Notify heading changes when heading is > 5.
+     * Default value is kCLHeadingFilterNone: all movements are reported.
+     */
+    self.locationManager.headingFilter = 5;
+    
+    if ([CLLocationManager locationServicesEnabled])
+    {
+        [self.locationManager startUpdatingLocation];
     }
 }
 
@@ -100,16 +149,16 @@
 
 - (void)zoomToUserLocation:(MKUserLocation *)userLocation
 {
-//    if (!userLocation)
-//    {
-//        return;
-//    }
-//    
-//    MKCoordinateRegion region;
-//    region.center = userLocation.location.coordinate;
-//    region.span = MKCoordinateSpanMake(1.0, 1.0);
-//    region = [self.mapView regionThatFits:region];
-//    [self.mapView setRegion:region animated:YES];
+    //    if (!userLocation)
+    //    {
+    //        return;
+    //    }
+    //
+    //    MKCoordinateRegion region;
+    //    region.center = userLocation.location.coordinate;
+    //    region.span = MKCoordinateSpanMake(1.0, 1.0);
+    //    region = [self.mapView regionThatFits:region];
+    //    [self.mapView setRegion:region animated:YES];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath
@@ -119,7 +168,7 @@
 {
     if ([self.mapView showsUserLocation])
     {
-
+        
     }
 }
 
@@ -142,11 +191,64 @@
     [self.mapView setRegion:region animated:YES];
 }
 
+//- (void)locationManager:(CLLocationManager *)manager
+//    didUpdateToLocation:(CLLocation *)newLocation
+//           fromLocation:(CLLocation *)oldLocation
+//{
+//    MKCoordinateRegion region = { { 0.0f, 0.0f }, { 0.0f, 0.0f } };
+//    region.center = newLocation.coordinate;
+//    region.span.longitudeDelta = 0.15f;
+//    region.span.latitudeDelta = 0.15f;
+//    [self.mapView setRegion:region animated:YES];
+//}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    if (oldLocation == nil) return;
+    //BOOL isStaleLocation = [oldLocation.timestamp compare:self.startTimestamp] == NSOrderedAscending;
+    
+    [self.distanceTravelled setText:[NSString stringWithFormat:@"accuracy: %.2f", newLocation.horizontalAccuracy]];
+    
+    if ( newLocation.horizontalAccuracy >= 0.0f && newLocation.horizontalAccuracy < kRequiredHorizontalAccuracy) {
+        
+        [self.locationHistory addObject:newLocation];
+        if ([self.locationHistory count] > kNumLocationHistoriesToKeep) {
+            [self.locationHistory removeObjectAtIndex:0];
+        }
+        
+        BOOL canUpdateDistance = NO;
+        if ([self.locationHistory count] >= kMinLocationsNeededToUpdateDistance) {
+            canUpdateDistance = YES;
+        }
+        
+        if ([NSDate timeIntervalSinceReferenceDate] - lastDistanceCalculation > kDistanceCalculationInterval) {
+            lastDistanceCalculation = [NSDate timeIntervalSinceReferenceDate];
+            
+            CLLocation *lastLocation = (lastRecordedLocation != nil) ? lastRecordedLocation : oldLocation;
+            
+            CLLocation *bestLocation = nil;
+            CGFloat bestAccuracy = kRequiredHorizontalAccuracy;
+            for (CLLocation *location in self.locationHistory) {
+                if ([NSDate timeIntervalSinceReferenceDate] - [location.timestamp timeIntervalSinceReferenceDate] <= kValidLocationHistoryDeltaInterval) {
+                    if (location.horizontalAccuracy < bestAccuracy && location != lastLocation) {
+                        bestAccuracy = location.horizontalAccuracy;
+                        bestLocation = location;
+                    }
+                }
+            }
+            if (bestLocation == nil) bestLocation = newLocation;
+            
+            CLLocationDistance distance = [bestLocation distanceFromLocation:lastLocation];
+            if (canUpdateDistance) totalDistance += distance;
+            lastRecordedLocation = bestLocation;
+        }
+    }
+}
+
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
-    if(self.firstTime)
+    if (self.firstTime)
     {
-        CLLocation *startingLocation = [locations objectAtIndex:0];
+        startingLocation = [locations objectAtIndex:0];
         
         MKPointAnnotation *startingPointAnnotation = [[MKPointAnnotation alloc] init];
         startingPointAnnotation.title = @"Starting Point";
@@ -160,15 +262,20 @@
     [self.locations addObject:[locations objectAtIndex:0]];
     
     CLLocationCoordinate2D coordinates[[self.locations count]];
-    for(int i = 0; i < self.locations.count; i++)
+    
+    for (int i = 0; i < self.locations.count; i++)
     {
-        CLLocation *currentLocation = [self.locations objectAtIndex:i];
-        coordinates[i] = currentLocation.coordinate;
+        lastRecordedLocation = [self.locations objectAtIndex:i];
+        coordinates[i] = lastRecordedLocation.coordinate;
     }
+    
     [self.mapView removeOverlays:self.mapView.overlays];
     
     MKPolyline *pathPolyline = [MKPolyline polylineWithCoordinates:coordinates count:self.locations.count];
     [self.mapView addOverlay:pathPolyline];
+//    totalDistance = [currentLocation distanceFromLocation:startingLocation];    //meters
+    
+//    [self.distanceTravelled setText:[NSString stringWithFormat:@"Distance travelled:%f", totalDistance]];
 }
 
 - (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay
@@ -176,9 +283,10 @@
     if([overlay isKindOfClass:[MKPolyline class]])
     {
         MKPolylineRenderer *polylineRenderer = [[MKPolylineRenderer alloc] initWithPolyline:overlay];
-        polylineRenderer.fillColor = [[UIColor redColor] colorWithAlphaComponent:0.2];
-        polylineRenderer.strokeColor = [[UIColor redColor] colorWithAlphaComponent:0.7];
-        polylineRenderer.lineWidth = 2.0;
+        polylineRenderer.fillColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.2];
+        polylineRenderer.strokeColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.7];
+        polylineRenderer.lineWidth = 3.0;
+        
         return polylineRenderer;
     }
     else
@@ -194,13 +302,14 @@
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
-    NSLog(@"Error while getting core location : %@",[error localizedFailureReason]);
-    if ([error code] == kCLErrorDenied)
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied)
     {
-        //you had denied
+        NSLog(@"User has denied location services");
     }
-    
-    [manager stopUpdatingLocation];
+    else
+    {
+        NSLog(@"Location manager did fail with error: %@", error.localizedFailureReason);
+    }
 }
 
 @end

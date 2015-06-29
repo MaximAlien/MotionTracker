76f569fa541b011c3119a942c4518681b07e31c9
Maxim Makhun <maxim.makhun@gmail.com>
2015-06-29 18:18:07 +0300

    Integrated drawer view controller

---
+++ MotionTracker/SidebarTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SidebarTableViewController.h
+//  MotionTracker
+//
+//  Created by Maxim Makhun on 6/29/15.
+//  Copyright (c) 2015 MMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SidebarTableViewController : UITableViewController
+
+@end

019b9d4182f6b17f0be393759ee3204f5c888625
Maxim Makhun <maxim.makhun@gmail.com>
2015-08-22 18:28:53 +0300

    Submitted missing changes.

--- MotionTracker/MainViewController.m
+++ MotionTracker/MainViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "MainViewController.h"
-#import "DayActivityItem.h"
 #import "CurrentActivityTableViewCell.h"
 #import "SWRevealViewController.h"
 #import "TodayActivityTableViewCell.h"
@@ -37,6 +36,28 @@ static int daysCounter = 8;
 - (void)dealloc
 {
     [self stopFollowingScrollView];
+}
+
+- (BOOL)coreDataHasEntriesForEntityName:(NSString *)entityName
+{
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:entityName inManagedObjectContext:self.managedObjectContext];
+    [request setEntity:entity];
+    [request setFetchLimit:1];
+    
+    NSError *error = nil;
+    NSArray *results = [self.managedObjectContext executeFetchRequest:request error:&error];
+    if (!results)
+    {
+        abort();
+    }
+    
+    if ([results count] == 0)
+    {
+        return NO;
+    }
+    
+    return YES;
 }
 
 - (void)viewDidLoad
@@ -71,70 +92,75 @@ static int daysCounter = 8;
         [dateComponents setDay:dateComponents.day - daysCounter];
         currentDate = [gregorianCalendar dateFromComponents:dateComponents];
         
-        for (int i = daysCounter; i >= 0; --i)
+        if (![self coreDataHasEntriesForEntityName:@"Day"])
         {
-            [dateComponents setDay:dateComponents.day + 1];
-            NSDate *nextDate = [gregorianCalendar dateFromComponents:dateComponents];
-
-            // NSLog(@"Date: %@", nextDate);
-
-            [self.pedometer queryPedometerDataFromDate:currentDate toDate:nextDate withHandler:^(CMPedometerData *pedometerData, NSError *error)
+            for (int i = daysCounter; i >= 0; --i)
             {
-                DayActivityItem *item = [[DayActivityItem alloc] init];
-                item.numberOfSteps = pedometerData.numberOfSteps;
-                item.distance = pedometerData.distance;
-                item.floorsAscended = pedometerData.floorsAscended;
-                item.floorsDescended = pedometerData.floorsDescended;
-                item.date = currentDate;
+                [dateComponents setDay:dateComponents.day + 1];
+                NSDate *nextDate = [gregorianCalendar dateFromComponents:dateComponents];
                 
-                [self.activityHistoryArray addObject:item];
+                [self.pedometer queryPedometerDataFromDate:currentDate toDate:nextDate withHandler:^(CMPedometerData *pedometerData, NSError *error)
+                 {
+                     NSManagedObject *day = [NSEntityDescription insertNewObjectForEntityForName:@"Day" inManagedObjectContext:[self managedObjectContext]];
+                     [day setValue:pedometerData.distance forKey:@"distance"];
+                     [day setValue:pedometerData.numberOfSteps forKey:@"steps"];
+                     [day setValue:currentDate forKey:@"date"];
+                     
+                     NSError *errorWrite = nil;
+                     
+                     if (![[self managedObjectContext] save:&errorWrite])
+                     {
+                         NSLog(@"Can't Save! %@ %@", errorWrite, [errorWrite localizedDescription]);
+                     }
+                     
+                     if (i == 0)
+                     {
+                         [self performSelectorOnMainThread:@selector(reloadTableView) withObject:nil waitUntilDone:NO];
+                     }
+                 }];
                 
-                [self saveDay:item];
-                
-                // NSLog(@"Steps count = %@, Distance = %@, Floors asc. = %@, Floors desc. = %@", pedometerData.numberOfSteps, pedometerData.distance, pedometerData.floorsAscended, pedometerData.floorsDescended);
-                
-                if (i == 0)
-                {
-                    [self performSelectorOnMainThread:@selector(reloadTableView) withObject:nil waitUntilDone:NO];
-                }
-            }];
-            
-            currentDate = nextDate;
+                currentDate = nextDate;
+            }
         }
         
-        NSDate *todayDate = [NSDate date];
-        dateComponents = [gregorianCalendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour fromDate:todayDate];
-        [dateComponents setHour:0];
-        todayDate = [gregorianCalendar dateFromComponents:dateComponents];
-        
-        [self.pedometer startPedometerUpdatesFromDate:todayDate withHandler:^(CMPedometerData *pedometerData, NSError *error)
-         {
-             DayActivityItem *item = [[DayActivityItem alloc] init];
-             item.numberOfSteps = pedometerData.numberOfSteps;
-             item.distance = pedometerData.distance;
-             item.floorsAscended = pedometerData.floorsAscended;
-             item.floorsDescended = pedometerData.floorsDescended;
-//             item.date = todayDate;
-             
-             if (self.activityHistoryArray.count != 0)
-             {
-                 [self.activityHistoryArray replaceObjectAtIndex:self.activityHistoryArray.count - 1 withObject:item];
-             }
-
-             NSLog(@"Steps count = %@, Distance = %@, Floors asc. = %@, Floors desc. = %@", pedometerData.numberOfSteps, pedometerData.distance, pedometerData.floorsAscended, pedometerData.floorsDescended);
-             
-             
-             [self performSelectorOnMainThread:@selector(reloadTableView) withObject:nil waitUntilDone:NO];
-         }];
+//        NSDate *todayDate = [NSDate date];
+//        dateComponents = [gregorianCalendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour fromDate:todayDate];
+//        [dateComponents setHour:0];
+//        todayDate = [gregorianCalendar dateFromComponents:dateComponents];
+//        
+//        [self.pedometer startPedometerUpdatesFromDate:todayDate withHandler:^(CMPedometerData *pedometerData, NSError *error)
+//         {
+//             DayActivityItem *item = [[DayActivityItem alloc] init];
+//             item.numberOfSteps = pedometerData.numberOfSteps;
+//             item.distance = pedometerData.distance;
+//             item.floorsAscended = pedometerData.floorsAscended;
+//             item.floorsDescended = pedometerData.floorsDescended;
+//             //             item.date = todayDate;
+//             
+//             if (self.activityHistoryArray.count != 0)
+//             {
+//                 [self.activityHistoryArray replaceObjectAtIndex:self.activityHistoryArray.count - 1 withObject:item];
+//             }
+//             
+//             NSLog(@"Steps count = %@, Distance = %@, Floors asc. = %@, Floors desc. = %@", pedometerData.numberOfSteps, pedometerData.distance, pedometerData.floorsAscended, pedometerData.floorsDescended);
+//             
+//             
+//             [self performSelectorOnMainThread:@selector(reloadTableView) withObject:nil waitUntilDone:NO];
+//         }];
     }
     else
     {
         NSLog(@"Data not available");
     }
+    
+    [self reloadTableView];
 }
 
 - (void)reloadTableView
 {
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Day"];
+    self.activityHistoryArray = [[[self managedObjectContext] executeFetchRequest:fetchRequest error:nil] mutableCopy];
+    
     [self.tableView reloadData];
 }
 
@@ -201,7 +227,8 @@ static int daysCounter = 8;
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     NSInteger row = self.activityHistoryArray.count - indexPath.row - 1;
-    DayActivityItem *item = (DayActivityItem *)[self.activityHistoryArray objectAtIndex:row];
+    
+    NSManagedObject *day = [self.activityHistoryArray objectAtIndex:row];
     
     if (indexPath.row == 0)
     {
@@ -215,11 +242,12 @@ static int daysCounter = 8;
             cell = [nib objectAtIndex:0];
         }
         
-        CGFloat res =  (item.numberOfSteps.floatValue * 100.0f) / [MainApp getDailyGoalStepsCounter];
+        float steps = [[day valueForKey:@"steps"] floatValue];
+        CGFloat res =  (steps * 100.0f) / [MainApp getDailyGoalStepsCounter];
         [cell.activityProgressView setProgress:res / 100.f];
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
-        [cell updateDailyProgressWithStepsCount:item.numberOfSteps];
-        cell.titleLabel.text = [NSString stringWithFormat:@"%@ steps", item.numberOfSteps];
+        [cell updateDailyProgressWithStepsCount:[NSNumber numberWithFloat:steps]];
+        cell.titleLabel.text = [NSString stringWithFormat:@"%@ steps", [day valueForKey:@"steps"]];
         
         return cell;
     }
@@ -235,12 +263,12 @@ static int daysCounter = 8;
             cell = [nib objectAtIndex:0];
         }
         
-        cell.numberOfStepsLabel.text = [NSString stringWithFormat:@"%@", item.numberOfSteps];
+        cell.numberOfStepsLabel.text = [NSString stringWithFormat:@"%@", [day valueForKey:@"steps"]];
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
         
         if (indexPath.row != 0)
         {
-            [cell updateDailyProgressWithStepsCount:item.numberOfSteps];
+            [cell updateDailyProgressWithStepsCount:[day valueForKey:@"steps"]];
         }
         
         return cell;
@@ -251,7 +279,7 @@ static int daysCounter = 8;
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
+    
 }
 
 // CoreData
@@ -266,23 +294,6 @@ static int daysCounter = 8;
     }
     
     return context;
-}
-
-- (void)saveDay:(DayActivityItem *)item
-{
-    NSManagedObjectContext *context = [self managedObjectContext];
-    
-    NSManagedObject *day = [NSEntityDescription insertNewObjectForEntityForName:@"Day" inManagedObjectContext:context];
-    [day setValue:item.distance forKey:@"distance"];
-    [day setValue:item.numberOfSteps forKey:@"steps"];
-    [day setValue:item.date forKey:@"date"];
-    
-    NSError *error = nil;
-
-    if (![context save:&error])
-    {
-        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
-    }
 }
 
 @end
